d4e6b31affdf06589a9096e8f7265240dc994874
Zineb Ait Moussa <aitmoussazineb@gmail.com>
2019-12-16 17:28:55 +0100

    first

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
-import {MatListModule} from '@angular/material/list';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -10,25 +8,16 @@ import {MatListModule} from '@angular/material/list';
 })
 export class GalleryComponent implements OnInit {
   pagePhoto: any;
-  size = 10 ;
-  curPage = 1;
-  pageOfItems: Array<number>;
-   totalPages: number;
   constructor(private http: HttpClient) {
     this.onSearch().subscribe(data => {
       this.pagePhoto = data;
       console.log(data);
-      this.totalPages = data.total_count / this.size;
-      if (data.total_count % this.size !== 0) {
-        ++this.totalPages;
-      }
-      this.pageOfItems = new Array(this.totalPages);
     });
   }
   ngOnInit() { }
 
   onSearch(): Observable<any> {
     // tslint:disable-next-line:max-line-length
-    return this.http.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&per_page=' + this.size + '&page=' + this.curPage);
+    return this.http.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&per_page' );
   }
 }
